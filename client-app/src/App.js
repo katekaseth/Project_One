716af1ce60ab47f511f55013ec0713dd172e1eed
Kateka Seth <katekaseth@gmail.com>
2020-02-16 16:14:21 -0800

    add basic wireframe

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.css";
 
-import Navbar from "./components/navbar";
-//import LandingPage from "./landing_page";
-import LandingPage from './components/landingPage/LandingPage';
+import LandingPage from "./components/landingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <LandingPage/>
+      <LandingPage />
     </div>
   );
 }
--- client-app/src/components/landingPage/LandingPage.js
+++ client-app/src/components/landingPage/LandingPage.js
@@ -1,38 +1,139 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
+import React from "react";
+import { Grid, Card, CardContent, makeStyles } from "@material-ui/core";
+import "./LandingPage.css";
+import hr_icon from "../../icons/hr.png";
+import kateka from "../../images/kateka.png";
 
-import { SubjectAreaCards } from './SubjectAreaCards';
-import { SearchBar } from '../SearchBar';
-import { CapstoneLandingInfo } from './CapstoneLandingInfo';
+import { CapstoneLandingInfo } from "./CapstoneLandingInfo";
+
+const useStyles = makeStyles({
+  img: {
+    borderRadius: "50%",
+    width: "50%",
+    height: "50%"
+  }
+});
 
 export default function LandingPage() {
-    return (
-        <Grid 
-            container 
-            justify='center'
-            alignItems='center'
-            id='landing-page'
-            spacing={10}
-            direction='column'
-        >
-            <Grid item className='title-header'>
-                <h1>Business Intelligence Portal</h1>
-            </Grid>
-            <Grid 
-                item
-                className='search-bar-container'>
-                <SearchBar/>
-            </Grid>
-            <Grid 
-                item
-                container 
-                justify='center'
-                className='subject-card-container'>
-                <SubjectAreaCards/>
-            </Grid>
-            <Grid xs={8} item className='capstone-info'>
-                <CapstoneLandingInfo/>
-            </Grid>
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh" }}>
+        <Grid item xs={4}>
+          <h1 className="title">UW Analytics</h1>
         </Grid>
-    )
+
+        <Grid item xs={6}>
+          <p className="subtitle">
+            We are creating a consolidated data platform that enables UW
+            academic and administrative units to efficiently and easily navigate
+            the University’s data so they can achieve more with analytics.
+          </p>
+        </Grid>
+      </Grid>
+
+      <Grid container direction="row">
+        <Grid item xs={4}>
+          <Card>
+            <CardContent>
+              <img src={hr_icon}></img>
+              <p>helloood askdlfjale lsdjfoieuwpar aslkdfjaei </p>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card>
+            <CardContent>
+              <img src={hr_icon}></img>
+              <p>helloood askdlfjale lsdjfoieuwpar aslkdfjaei </p>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card>
+            <CardContent>
+              <img src={hr_icon}></img>
+              <p>helloood askdlfjale lsdjfoieuwpar aslkdfjaei </p>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+
+      <Grid container alignItems="center" justify="center">
+        <Grid item>
+          <h2 className="title2">We're making your job easier.</h2>
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justify="center"
+        spacing="5">
+        <Grid item xs={3}>
+          <Card>
+            <CardContent>
+              <img src={kateka} className={classes.img}></img>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                repellendus quasi fuga nesciunt dolorum nulla magnam veniam
+                sapiente, fugiat! Commodi sequi non animi ea dolor molestiae
+                iste.
+              </p>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={3}>
+          <Card>
+            <CardContent>
+              <img src={kateka} className={classes.img}></img>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                repellendus quasi fuga nesciunt dolorum nulla magnam veniam
+                sapiente, fugiat! Commodi sequi non animi ea dolor molestiae
+                iste.
+              </p>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={3}>
+          <Card>
+            <CardContent>
+              <img src={kateka} className={classes.img}></img>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                repellendus quasi fuga nesciunt dolorum nulla magnam veniam
+                sapiente, fugiat! Commodi sequi non animi ea dolor molestiae
+                iste.
+              </p>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={3}>
+          <Card>
+            <CardContent>
+              <img src={kateka} className={classes.img}></img>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                repellendus quasi fuga nesciunt dolorum nulla magnam veniam
+                sapiente, fugiat! Commodi sequi non animi ea dolor molestiae
+                iste.
+              </p>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+
+      <CapstoneLandingInfo></CapstoneLandingInfo>
+    </div>
+  );
 }
