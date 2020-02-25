dcbe2eff5fc1ae82bb60eb7e7d5e4695fec406cd
Kateka Seth <katekaseth@gmail.com>
2020-02-24 21:57:02 -0800

    fix deploy script

--- server/build.sh
+++ server/build.sh
@@ -1,8 +1,15 @@
+#!/bin/bash 
+
 CGO_ENABLED=0 go build
 docker build -t kateks/capstone_api .
 docker push kateks/capstone_api
 go clean
 
-# cd db
-# docker build -t kateks/capstone_store .
-# docker push kateks/capstone_store
+TYPE=$1
+
+if [ "$TYPE" == "db" ]
+then
+    cd db
+    docker build -t kateks/capstone_store .
+    docker push kateks/capstone_store
+fi
