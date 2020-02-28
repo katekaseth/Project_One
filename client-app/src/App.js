2850077d7021efeea4cfadb0577401788589d9cc
Kateka Seth <katekaseth@gmail.com>
2020-02-27 19:57:16 -0800

    merge

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,47 +1,43 @@
-import React, { useState } from 'react';
-import {
-    Switch,
-    Route,
-    useHistory,
-} from 'react-router-dom';
+import React, { useState } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
 
-import { PAGES } from './stringConstants';
-import Navbar from './components/Navbar';
-import LandingPage from './components/landingPage/LandingPage';
-import SearchPage from './components/searchPage/SearchPage';
-import ResultPage from './components/resultPage/ResultPage';
+import { PAGES } from "./stringConstants";
+import Navbar from "./components/navbar";
+import LandingPage from "./components/landingPage/LandingPage";
+import SearchPage from "./components/searchPage/SearchPage";
+import ResultPage from "./components/resultPage/ResultPage";
 
 function App() {
-    const [page, setPage] = useState('/');
-    const history = useHistory();
+  const [page, setPage] = useState("/");
+  const history = useHistory();
 
-    const GLOBAL_STATE = {
-        page: page,
-    };
+  const GLOBAL_STATE = {
+    page: page
+  };
 
-    const GLOBAL_ACTIONS = {
-        setPage: (page) => {
-            setPage(page);
-            history.push(page);
-        },
-    };
+  const GLOBAL_ACTIONS = {
+    setPage: page => {
+      setPage(page);
+      history.push(page);
+    }
+  };
 
   return (
-    <div className='app'> 
-        <Navbar {...GLOBAL_ACTIONS} transparent={page === PAGES.home} />
-        <Switch>
-            <Route exact path={PAGES.home}>
-                <div className='landing-page-container'>
-                    <LandingPage {...GLOBAL_ACTIONS}/>
-                </div>
-            </Route>
-            <Route path={PAGES.search}>
-                <SearchPage {...GLOBAL_ACTIONS}/>
-            </Route>
-            <Route path={PAGES.result}>
-                <ResultPage {...GLOBAL_ACTIONS}/>
-            </Route> 
-        </Switch>
+    <div className="app">
+      <Navbar {...GLOBAL_ACTIONS} transparent={page === PAGES.home} />
+      <Switch>
+        <Route exact path={PAGES.home}>
+          <div className="landing-page-container">
+            <LandingPage {...GLOBAL_ACTIONS} />
+          </div>
+        </Route>
+        <Route path={PAGES.search}>
+          <SearchPage {...GLOBAL_ACTIONS} />
+        </Route>
+        <Route path={PAGES.result}>
+          <ResultPage {...GLOBAL_ACTIONS} />
+        </Route>
+      </Switch>
     </div>
   );
 }
