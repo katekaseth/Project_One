58b429669f98ea1d287d3be06229805a203084a4
Amelia Shull <ameliashull@ameliashull.local>
2020-04-16 17:17:09 -0700

    add sign up, sign out

--- client-app/src/components/loginPage/LoginPage.js
+++ client-app/src/components/loginPage/LoginPage.js
@@ -5,78 +5,105 @@ import { Grid, Typography, Divider, TextField, Button, CardMedia, Box } from '@m
 import wLogo from '../../images/W-Logo.png';
 import wordmarkLogo from '../../images/Wordmark-Stacked.png';
 
-export default ({ login }) => {
+export default ({ login, createAccount }) => {
     const classes = useStyles();
 
     return (
         <Grid container direction='column' alignItems='center' className={classes.content}>
             <Grid item container justify='center'>
                 <Grid xs item container alignItems='flex-end' direction='column'>
-                    <LoginComponent login={login} />
+                    <AccountComponent login={login} createAccount={createAccount} />
                 </Grid>
                 <Divider orientation='vertical' flexItem />
-                <Grid xs item container direction='column' className={classes.learnLinks}>
-                    <Typography className={classes.link} variant='body2'>
-                        Learn about account recovery options
-                    </Typography>
-                    <Typography className={classes.link} variant='body2'>
-                        Learn about UW NetIDs
-                    </Typography>
-                    <Typography className={classes.link} variant='body2'>
-                        Learn about UW NetID sign-in
-                    </Typography>
-                    <Typography className={classes.link} variant='body2'>
-                        Obtain a UW NetID
-                    </Typography>
-                    <br />
-                    <Typography className={classes.link} variant='body2'>
-                        Need help?
-                    </Typography>
-                </Grid>
+                <RightLinks />
             </Grid>
-            <Grid
-                item
-                container
-                direction='column'
-                alignItems='center'
-                className={classes.bottomContent}
-            >
-                <Typography variant='body2'>
-                    Sign in reduces how often you have to reauthenticate to access UW resources.
-                </Typography>
-                <br />
-                <Typography variant='body2'>
-                    Learn how to{' '}
-                    <Box className={classes.link} component='span'>
-                        sign out
-                    </Box>{' '}
-                    at the end of your browsing session.
-                </Typography>
-                <br />
-                <Typography variant='body2'>
-                    <Box className={classes.link} component='span'>
-                        PRIVACY
-                    </Box>
-                    <Box component='span'> | </Box>
-                    <Box className={classes.link} component='span'>
-                        TERMS
-                    </Box>
-                </Typography>
-            </Grid>
+            <BottomLinks />
         </Grid>
     );
 };
 
-const LoginComponent = ({ login }) => {
+const AccountComponent = ({ login, createAccount }) => {
     const classes = useStyles();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordConf, setPasswordConf] = useState('');
+
+    const [createAccountMode, setCreateAccountMode] = useState(false);
 
     const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
+        if (event.key === 'Enter' && createAccountMode) {
+            createAccount(username, password, passwordConf);
+        } else if (event.key === 'Enter') {
             login(username, password);
         }
     };
+
+    const loginComponents = (
+        <Grid>
+            <Grid className={classes.items}>
+                <Typography className={classes.link} variant='body2'>
+                    Forgot your password?
+                </Typography>
+            </Grid>
+
+            <Grid item className={classes.items}>
+                <Button
+                    variant='contained'
+                    color='primary'
+                    onClick={() => login(username, password)}
+                >
+                    Sign in
+                </Button>
+            </Grid>
+
+            <Grid item className={classes.items}>
+                <Typography variant='body2'>Don't have an account?</Typography>
+                <Typography
+                    variant='body2'
+                    className={classes.link}
+                    onClick={() => setCreateAccountMode(true)}
+                >
+                    Create account
+                </Typography>
+            </Grid>
+        </Grid>
+    );
+
+    const createAccountComponents = (
+        <Grid>
+            <Grid item className={classes.items}>
+                <TextField
+                    onKeyPress={handleKeyPress}
+                    onChange={(e) => setPasswordConf(e.target.value)}
+                    size='small'
+                    label='Type Password Again'
+                    type='password'
+                    variant='outlined'
+                />
+            </Grid>
+
+            <Grid item className={classes.items}>
+                <Button
+                    variant='contained'
+                    color='primary'
+                    onClick={() => createAccount(username, password, passwordConf)}
+                >
+                    Create account
+                </Button>
+            </Grid>
+
+            <Grid item className={classes.items}>
+                <Typography variant='body2'>Already have an account?</Typography>
+                <Typography
+                    variant='body2'
+                    className={classes.link}
+                    onClick={() => setCreateAccountMode(false)}
+                >
+                    Sign in
+                </Typography>
+            </Grid>
+        </Grid>
+    );
 
     return (
         <Grid item container direction='column' xs={4}>
@@ -86,7 +113,9 @@ const LoginComponent = ({ login }) => {
             </Grid>
 
             <Grid item className={classes.items}>
-                <Typography variant='body2'>Please sign in.</Typography>
+                <Typography variant='body2'>
+                    {createAccountMode ? 'Create an account.' : 'Please sign in.'}
+                </Typography>
             </Grid>
 
             <Grid item className={classes.items}>
@@ -109,22 +138,66 @@ const LoginComponent = ({ login }) => {
                     variant='outlined'
                 />
             </Grid>
+            {createAccountMode ? createAccountComponents : loginComponents}
+        </Grid>
+    );
+};
 
-            <Grid className={classes.items}>
-                <Typography className={classes.link} variant='body2'>
-                    Forgot your password?
-                </Typography>
-            </Grid>
+const RightLinks = () => {
+    const classes = useStyles();
+    return (
+        <Grid xs item container direction='column' className={classes.learnLinks}>
+            <Typography className={classes.link} variant='body2'>
+                Learn about account recovery options
+            </Typography>
+            <Typography className={classes.link} variant='body2'>
+                Learn about UW NetIDs
+            </Typography>
+            <Typography className={classes.link} variant='body2'>
+                Learn about UW NetID sign-in
+            </Typography>
+            <Typography className={classes.link} variant='body2'>
+                Obtain a UW NetID
+            </Typography>
+            <br />
+            <Typography className={classes.link} variant='body2'>
+                Need help?
+            </Typography>
+        </Grid>
+    );
+};
 
-            <Grid item className={classes.items}>
-                <Button
-                    variant='contained'
-                    color='primary'
-                    onClick={() => login(username, password)}
-                >
-                    Sign in
-                </Button>
-            </Grid>
+const BottomLinks = () => {
+    const classes = useStyles();
+    return (
+        <Grid
+            item
+            container
+            direction='column'
+            alignItems='center'
+            className={classes.bottomContent}
+        >
+            <Typography variant='body2'>
+                Sign in reduces how often you have to reauthenticate to access UW resources.
+            </Typography>
+            <br />
+            <Typography variant='body2'>
+                Learn how to{' '}
+                <Box className={classes.link} component='span'>
+                    sign out
+                </Box>{' '}
+                at the end of your browsing session.
+            </Typography>
+            <br />
+            <Typography variant='body2'>
+                <Box className={classes.link} component='span'>
+                    PRIVACY
+                </Box>
+                <Box component='span'> | </Box>
+                <Box className={classes.link} component='span'>
+                    TERMS
+                </Box>
+            </Typography>
         </Grid>
     );
 };
