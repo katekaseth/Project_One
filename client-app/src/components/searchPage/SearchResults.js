40f132c8c79188d75bb77535c125d66b4c6804ed
Amelia Shull <ameliashull@ameliashull.local>
2020-04-21 18:38:11 -0700

    fix bug

--- client-app/src/components/searchPage/SearchResults.js
+++ client-app/src/components/searchPage/SearchResults.js
@@ -8,21 +8,24 @@ import { SearchResult } from './SearchResult';
 const NUM_PER_PAGE = 5;
 
 export const SearchResults = ({ setPage, results, alertError }) => {
-    const [pagination, setPagination] = useState(0);
+    const [pagination, setPagination] = useState(1);
     const classes = useStyles();
 
     if (results === null) return <div></div>;
-    let numOfPages = parseInt(results.length / NUM_PER_PAGE);
+    let numOfPages = Math.ceil(results.length / NUM_PER_PAGE);
+
+    let resultCards = results.map((result, index) => {
+        return (
+            <SearchResult key={index} setPage={setPage} result={result} alertError={alertError} />
+        );
+    });
 
     return (
         <Grid container direction='column' className='search-result-container' alignItems='center'>
-            {results
-                .slice(pagination * NUM_PER_PAGE, (pagination + 1) * NUM_PER_PAGE)
-                .map((result) => {
-                    return (
-                        <SearchResult setPage={setPage} result={result} alertError={alertError} />
-                    );
-                })}
+            {resultCards.slice(
+                (pagination - 1) * NUM_PER_PAGE,
+                (pagination - 1) * NUM_PER_PAGE + NUM_PER_PAGE,
+            )}
             {numOfPages > 1 && (
                 <Pagination
                     className={classes.pagination}
