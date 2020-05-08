02ddc99bd039028b100cfd8e690e34f7b66b0bdc
Amelia Shull <ameliashull@ameliashull.local>
2020-05-07 17:40:26 -0700

    sort

--- client-app/src/components/searchPage/SearchPage.js
+++ client-app/src/components/searchPage/SearchPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, MenuItem, Select, InputLabel, FormControl } from '@material-ui/core';
 
 import { SearchBar } from '../SearchBar';
 import { SearchFilter } from './SearchFilter';
@@ -9,11 +9,22 @@ import { SearchResults } from './SearchResults';
 import { FilterChips } from '../Chips';
 import { STANDARDIZED_CATEOGRY_KEYS } from '../../stringConstants';
 
+import { a_zSort, z_aSort, mostRecentSort, leastRecentSort } from './compareFunctions';
+
+const SORT_VALUES = {
+    A_Z: 'A_Z',
+    Z_A: 'Z_A',
+    MOST_RECENTLY_UPDATED: 'MOST_RECENTLY_UPDATED',
+    LEAST_RECENTLY_UPDATED: 'LEAST_RECENTLY_UPDATED',
+};
+
 export default (props) => {
     const classes = useStyles();
 
     let expandedObject = null;
     const [expandedFilterGroups, setExpandedFilterGroups] = useState(expandedObject);
+
+    const [sortedResults, setSortedResults] = useState(null);
 
     const changeFilterExpansion = (subjectKey) => {
         let temp = { ...expandedFilterGroups };
@@ -32,7 +43,24 @@ export default (props) => {
             });
             setExpandedFilterGroups(expandedObject);
         }
+        if (sortedResults === null && props.results !== null) {
+            setSortedResults(props.results);
+        }
     });
+
+    const handleSortChange = (sortBy) => {
+        if (sortBy === SORT_VALUES.A_Z) {
+            setSortedResults(props.results.sort(a_zSort).slice(0));
+        } else if (sortBy === SORT_VALUES.Z_A) {
+            setSortedResults(props.results.sort(z_aSort).slice(0));
+        } else if (sortBy === SORT_VALUES.MOST_RECENTLY_UPDATED) {
+            setSortedResults(props.results.sort(mostRecentSort).slice(0));
+        } else if (sortBy === SORT_VALUES.LEAST_RECENTLY_UPDATED) {
+            setSortedResults(props.results.sort(leastRecentSort).slice(0));
+        } else {
+            setSortedResults(props.results);
+        }
+    };
 
     if (props.filterState === null || expandedFilterGroups === null) return <div></div>;
 
@@ -52,15 +80,55 @@ export default (props) => {
                 <SearchBar redirect={() => props.setPage.search()} {...props} />
 
                 <Grid item container className={classes.filterChips}>
-                    <FilterChipDisplay
-                        nothingFound={props.results && props.results.length === 0}
-                        numResults={props.results && props.results.length}
-                        changeFilterExpansion={changeFilterExpansion}
-                        {...props}
+                    <Grid item container alignItems='flex-start'>
+                        <Grid item xs={9}>
+                            <FilterChipDisplay
+                                nothingFound={props.results && props.results.length === 0}
+                                numResults={props.results && props.results.length}
+                                changeFilterExpansion={changeFilterExpansion}
+                                {...props}
+                            />
+                        </Grid>
+                        <Grid item xs>
+                            <SortSelector handleSortChange={handleSortChange} />
+                        </Grid>
+                    </Grid>
+                    <SearchResults
+                        setPage={props.setPage}
+                        results={sortedResults}
+                        alertError={props.alertError}
                     />
-                    <SearchResults {...props} />
                 </Grid>
             </Grid>
+        </Grid>
+    );
+};
+
+const SortSelector = ({ handleSortChange }) => {
+    const classes = useStyles();
+    const handleChange = (event) => {
+        handleSortChange(event.target.value);
+    };
+
+    return (
+        <Grid className={classes.sortSelector}>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Sort By</InputLabel>
+                <Select onChange={handleChange}>
+                    <MenuItem value={SORT_VALUES.A_Z}>
+                        <Typography variant='body2'>Title: A-Z</Typography>
+                    </MenuItem>
+                    <MenuItem value={SORT_VALUES.Z_A}>
+                        <Typography variant='body2'>Title: Z-A</Typography>
+                    </MenuItem>
+                    <MenuItem value={SORT_VALUES.MOST_RECENTLY_UPDATED}>
+                        <Typography variant='body2'>Most recently updated</Typography>
+                    </MenuItem>
+                    <MenuItem value={SORT_VALUES.LEAST_RECENTLY_UPDATED}>
+                        <Typography variant='body2'>Least recently updated</Typography>
+                    </MenuItem>
+                </Select>
+            </FormControl>
         </Grid>
     );
 };
@@ -163,5 +231,12 @@ const useStyles = makeStyles({
     },
     nothingFound: {
         marginTop: '30px',
+    },
+    sortSelector: {
+        float: 'right',
+        marginTop: '-10px',
+    },
+    formControl: {
+        minWidth: '170px',
     },
 });
