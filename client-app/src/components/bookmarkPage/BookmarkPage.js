5764c810cb169f45648b437c752063283306fc86
Amelia Shull <ameliashull@ameliashull.local>
2020-05-11 12:19:00 -0700

    help on bookmark page

--- client-app/src/components/bookmarkPage/BookmarkPage.js
+++ client-app/src/components/bookmarkPage/BookmarkPage.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/styles';
 import { Typography, Grid, CardMedia } from '@material-ui/core';
 
 import bookmarkedIcon from '../../icons/svg/bookmarked.svg';
+import unbookmarkedIcon from '../../icons/svg/unbookmarked.svg';
 
 import { SearchResults } from '../searchPage/SearchResults';
 import { SearchBar } from '../SearchBar';
@@ -32,16 +33,57 @@ export default ({
                 </Grid>
             </Grid>
             <Grid item className={classes.bottomSpacing}>
-                <SearchBar
-                    redirect={() => {}}
-                    updateSearchTerms={updateSearchBookmarkTerms}
-                    searchedTerms={[searchedBookmarkTerms]}
-                    isBookmark={true}
-                ></SearchBar>
+                {bookmarks && bookmarks.length !== 0 && (
+                    <SearchBar
+                        redirect={() => {}}
+                        updateSearchTerms={updateSearchBookmarkTerms}
+                        searchedTerms={[searchedBookmarkTerms]}
+                        isBookmark={true}
+                    />
+                )}
             </Grid>
             <Grid item container>
-                <SearchResults setPage={setPage} results={bookmarks} alertError={alertError} />
+                {bookmarks && bookmarks.length === 0 && <NoBookmarksHelper />}
+                {bookmarks && bookmarks.length !== 0 && (
+                    <SearchResults setPage={setPage} results={bookmarks} alertError={alertError} />
+                )}
             </Grid>
+        </Grid>
+    );
+};
+
+const NoBookmarksHelper = () => {
+    const classes = useStyles();
+    return (
+        <Grid>
+            <Typography variant='h6'>Looks like you don't have any bookmarks!</Typography>
+            <Typography>
+                To add new bookmarks, look for this symbol{' '}
+                <CardMedia
+                    src={unbookmarkedIcon}
+                    component='img'
+                    className={classes.littleBookmark}
+                />{' '}
+                on a report overview. Click it to save the bookmark.
+            </Typography>
+            <br />
+            <Typography variant='body2'>
+                <CardMedia
+                    src={bookmarkedIcon}
+                    component='img'
+                    className={classes.littlestBookmark}
+                />{' '}
+                If the icon is purple, it means the report has been bookmarked. Click to unbookmark
+                it.
+            </Typography>
+            <Typography variant='body2'>
+                <CardMedia
+                    src={unbookmarkedIcon}
+                    component='img'
+                    className={classes.littlestBookmark}
+                />{' '}
+                If it's grey, it means it hasn't been bookmarked. Click to bookmark it.
+            </Typography>
         </Grid>
     );
 };
@@ -66,5 +108,19 @@ const useStyles = makeStyles({
     },
     bottomSpacing: {
         marginBottom: '1rem',
+    },
+    littleBookmark: {
+        width: '20px',
+        display: 'inline',
+        marginBottom: '-4px',
+        marginRight: '3px',
+        marginLeft: '3px',
+    },
+    littlestBookmark: {
+        width: '10px',
+        display: 'inline',
+        marginRight: '3px',
+        marginLeft: '3px',
+        marginTop: '5px',
     },
 });
