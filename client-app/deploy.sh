628eb70487d7475bf835432549847e01071c84a3
katekaseth <katekaseth@gmail.com>
2020-05-15 12:33:28 -0700

    change deploy to uwanalytics.site

--- client-app/deploy.sh
+++ client-app/deploy.sh
@@ -1,4 +1,4 @@
-docker build -t ashull/capstone-client .
-docker push ashull/capstone-client
+docker build -t kateks/capstone-client .
+docker push kateks/capstone-client
 
 ssh ec2-user@ec2-100-21-105-146.us-west-2.compute.amazonaws.com < deploy_commands.sh
--- client-app/deploy_commands.sh
+++ client-app/deploy_commands.sh
@@ -4,12 +4,12 @@ docker rm -f client
 docker system prune --all -f
 docker system prune --volumes -f
 
-docker pull ashull/capstone-client
+docker pull kateks/capstone-client
 docker run -d \
 -p 80:80 \
 -p 443:443 \
 --name client \
 -v /etc/letsencrypt/:/etc/letsencrypt/:ro \
--e TLSCERT=/etc/letsencrypt/live/katekaseth.me/fullchain.pem \
--e TLSKEY=/etc/letsencrypt/live/katekaseth.me/privkey.pem \
-ashull/capstone-client
+-e TLSCERT=/etc/letsencrypt/live/uwanalytics.site/fullchain.pem \
+-e TLSKEY=/etc/letsencrypt/live/uwanalytics.site/privkey.pem \
+kateks/capstone-client
